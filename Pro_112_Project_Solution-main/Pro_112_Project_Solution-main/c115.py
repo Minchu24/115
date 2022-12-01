bc7c29dee611f6c85d0bcefe4246f768b8b36a33
Minchu24 <87452028+Minchu24@users.noreply.github.com>
2022-11-30 19:31:42 -0800

    Add files via upload

---
+++ Pro_112_Project_Solution-main/Pro_112_Project_Solution-main/c115.py
@@ -0,0 +1,5 @@
+import os
+source = 'C:/Users/minchu/Downloads/Pro_112_Project_Solution-main/bcd.txt'
+
+dest ='C:/Users/minchu/Downloads/Pro_112_Project_Solution-main/123.txt'
+os.rename(source,dest)
